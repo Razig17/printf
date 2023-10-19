3d8de9d5faf38c69d7907a1a84708e96dacdda92
Razig17 <razog1999@gmail.com>
2023-10-19 02:13:12 -0700

    Razig

--- m_funcs.c
+++ m_funcs.c
@@ -61,9 +61,7 @@ int print_string(va_list args)
 	char *str = va_arg(args, char *);
 	int count = 0;
 
-	if (str == NULL)
-	return (-1);
-	else if (str == (char *)0)
+	if (str == (char *)0)
 		return (_printf("(null)"));
 
 	count = _strlen(str);
